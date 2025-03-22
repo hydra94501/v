d28ee0e1791cda971e0b88a3a281f135ebb6ea86
hydra94501 <hydra94501@gmail.com>
2025-03-22 15:21:23 +0800

    1
    1

---
+++ api/cp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 查找并杀死tomcat82进程
+pid=$(ps -ef | grep tomcat82 | grep -v grep | awk '{print $2}')
+if [ -n "$pid" ]; then
+    kill -9 $pid
+    echo "Tomcat82 process (PID: $pid) has been killed"
+else
+    echo "No tomcat82 process found"
+fi
+echo "start cp...."
+cp -rf /home/v/api/com   /home/x/tomcat82/webapps/ROOT/WEB-INF/classes/
+echo "end cp"
+
+echo "Tomcat82 is starting..."
+/home/x/tomcat82/bin/startup.sh
+
+echo "Tomcat82 is started !!!"
